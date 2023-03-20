76b8f69c9c70e83666c8d5f1a1e9f25f242b498c
Inon Bercu <inonbr@gmail.com>
2023-03-20 14:08:08 +0200

    created entitis

---
+++ src/entities/CompanyEntity.ts
@@ -0,0 +1,31 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+import { Order } from "./OrderEntity";
+
+@Entity()
+export class Company extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  companyId: string = uuidv4();
+
+  @Column({ nullable: false })
+  name!: string;
+
+  @Column({ nullable: false })
+  phone!: string;
+
+  @Column({ nullable: false })
+  address!: string;
+
+  @CreateDateColumn({ nullable: false })
+  creationDate: Date = new Date();
+
+  @OneToMany(() => Order, (order) => order.company)
+  orders!: Order[];
+}
---
+++ src/entities/OrderEntity.ts
@@ -0,0 +1,29 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+import { Company } from "./CompanyEntity";
+
+@Entity()
+export class Order extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  orderId: string = uuidv4();
+
+  @Column({ nullable: false })
+  employId: string = uuidv4();
+
+  @Column({ nullable: false })
+  item!: string;
+
+  @CreateDateColumn({ nullable: false })
+  orderDate: Date = new Date();
+
+  @ManyToOne(() => Company, (company) => company.orders, { cascade: true })
+  company!: Company;
+}
