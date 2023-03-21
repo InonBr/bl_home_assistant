82b96b0e225397f818fbab5dd3f480905641c281
Inon Bercu <inonbr@gmail.com>
2023-03-21 15:17:32 +0200

    added some css

--- client/src/components/tables/QueryTable.tsx
+++ client/src/components/tables/QueryTable.tsx
@@ -1,30 +1,33 @@
 import { Table } from "react-bootstrap";
 import { TableData } from "../interfaces/tableInterfaces";
+import "../styles/tables.css";
 
 const QueryTable = ({ tableData }: TableData) => {
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Phone</th>
-          <th>Address</th>
-          <th>Company ID</th>
-          <th>Creation Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tableData.map((item) => (
-          <tr key={item.companyId}>
-            <td>{item.name}</td>
-            <td>{item.phone}</td>
-            <td>{item.address}</td>
-            <td>{item.companyId}</td>
-            <td>{item.creationDate}</td>
+    <div className="table-container">
+      <Table striped bordered hover className="tables">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th>Company ID</th>
+            <th>Creation Date</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {tableData.map((item) => (
+            <tr key={item.companyId}>
+              <td>{item.name}</td>
+              <td>{item.phone}</td>
+              <td>{item.address}</td>
+              <td>{item.companyId}</td>
+              <td>{item.creationDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
