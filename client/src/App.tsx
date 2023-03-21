9657a276eccd26e545f9c12f931e1ac7ca40533d
Inon Bercu <inonbr@gmail.com>
2023-03-21 06:35:32 +0200

    created react app base app

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="m-5">hello world</h1>
+      <Button variant="primary">Primary</Button>{" "}
+    </div>
+  );
+}
+
+export default App;
